9b8175b056c0b18ea54a6e7fd4c36f5020206612
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-22 05:31:05 -0800

    Internalize `media_sideload_image()` to return attachment id

--- command.php
+++ command.php
@@ -41,10 +41,44 @@ class Fetch_WPCom {
 		}
 		$wpdb->update( $wpdb->posts, array( 'post_name' => $slug ), array( 'ID' => $post_id ) );
 		if ( ! empty( $body->featured_image ) ) {
-			$html = media_sideload_image( $body->featured_image, $post_id, '' );
-			WP_CLI::log( $html );
+			$attachment_id = self::media_sideload_image( $body->featured_image, $post_id );
+			if ( $attachment_id && ! is_wp_error( $attachment_id ) ) {
+				set_post_thumbnail( $post_id, $attachment_id );
+			}
 		}
 		WP_CLI::success( "Fetched {$slug} as post id {$post_id}." );
+	}
+
+	private static function media_sideload_image( $file, $post_id, $desc = null, $return = 'html' ) {
+		if ( ! empty( $file ) ) {
+			// Set variables for storage, fix file filename for query strings.
+			preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $file, $matches );
+			if ( ! $matches ) {
+				return new WP_Error( 'image_sideload_failed', __( 'Invalid image URL' ) );
+			}
+
+			$file_array = array();
+			$file_array['name'] = basename( $matches[0] );
+
+			// Download file to temp location.
+			$file_array['tmp_name'] = download_url( $file );
+
+			// If error storing temporarily, return the error.
+			if ( is_wp_error( $file_array['tmp_name'] ) ) {
+				return $file_array['tmp_name'];
+			}
+
+			// Do the validation and storage stuff.
+			$id = media_handle_sideload( $file_array, $post_id, $desc );
+
+			// If error storing permanently, unlink.
+			if ( is_wp_error( $id ) ) {
+				@unlink( $file_array['tmp_name'] );
+				return $id;
+			}
+			return $id;
+		}
+		return false;
 	}
 
 }
